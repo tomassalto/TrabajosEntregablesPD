697d88748d072e675a0e95656425e8068f81fdd3
Guillermo Vera <veraguillermo345@gmail.com>
2022-08-27 01:12:49 -0300

    Agregando archivos index

---
+++ TP1/indexTP1.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Indice TP1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    
+</head>
+<body>
+<table class="table table-striped table-hover" style="width:100%;">
+<thead>
+    <tr>
+      <th scope="col" style="text-align:center">Ejercicios TP 1</th>
+    </tr>
+</thead>
+<tbody>
+    <tr>
+        <td style="text-align:center">
+            <a href="Ej1/Vista/Ejercicio1.php">Ejercicio 1</a>
+        </td>
+    </tr>
+    <tr>
+        <td style="text-align:center">
+            <a href="Ej2/Vista/Ej2.php">Ejercicio 2</a>
+        </td>
+    </tr>
+    <tr>
+        <td style="text-align:center">
+            <a href="Ej3/Vista/Ej3.php">Ejercicio 3</a>
+        </td>
+    </tr>
+    <tr>
+        <td style="text-align:center">
+            <a href="Ej4/Vista/Ej4.php">Ejercicio 4</a>
+        </td>
+    </tr>
+    <tr>
+        <td style="text-align:center">
+            <a href="Ej5/Vista/Ej5.php">Ejercicio 5</a>
+        </td>
+    </tr>
+    <tr>
+        <td style="text-align:center">
+            <a href="Ej6/Vista/Ej6.php">Ejercicio 6</a>
+        </td>
+    </tr>
+    <tr>
+        <td style="text-align:center">
+            <a href="Ej7/Vista/Ej7.php">Ejercicio 7</a>
+        </td>
+    </tr>
+    <tr>
+        <td style="text-align:center">
+            <a href="Ej8/Vista/Ej8.php">Ejercicio 8</a>
+        </td>
+    </tr>
+    <tr>
+        <td style="text-align:center">
+            <a href="../index.php">Volver al menu principal.</a>
+        </td>
+    </tr>
+</tbody>
+</table>
+    
+</body>
+</html>
---
+++ tp3/indexTP3.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Indice TP3</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    
+</head>
+<body>
+<table class="table table-striped table-hover" style="width:100%;">
+<thead>
+    <tr>
+      <th scope="col" style="text-align:center">Ejercicios TP 2</th>
+    </tr>
+</thead>
+<tbody>
+    <tr>
+        <td style="text-align:center">
+            <a href="Ej1/Vista/Ej1.php">Ejercicio 1</a>
+        </td>
+    </tr>
+    <tr>
+        <td style="text-align:center">
+            <a href="Ej2/Vista/Ej2.php">Ejercicio 2</a>
+        </td>
+    </tr>
+    <tr>
+        <td style="text-align:center">
+            <a href="Ej3/Vista/Ej3.php">Ejercicio 3</a>
+        </td>
+    </tr>
+    <tr>
+        <td style="text-align:center">
+            <a href="Ej4/Vista/Ej4.php">Ejercicio 4</a>
+        </td>
+    </tr>
+</tbody>
+</table>
+    
+</body>
+</html>
